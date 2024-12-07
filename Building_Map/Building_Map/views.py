69dd3d0f0f4bf9b8dd9464839d427e966fe89954
jyparraguirre <jantroy23@gmail.com>
2024-12-07 18:01:39 +0800

    1st fix

--- Building_Map/Building_Map/views.py
+++ Building_Map/Building_Map/views.py
@@ -9,10 +9,17 @@ from Building_Map import app
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
     return render_template(
-        'index.html',
-        title='Home Page',
+        'index1.html',
+        title='1st Floor',
+        year=datetime.now().year,
+    )
+
+@app.route('/index2.cshtml')
+def index2():
+    return render_template(
+        'index2.html',
+        title='2nd Floor',
         year=datetime.now().year,
     )
 
@@ -35,3 +42,4 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
